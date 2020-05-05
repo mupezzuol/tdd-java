1e06ca9c40d12522fd4b50891ff876fdbe5cf81e
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-05-05 11:32:01 +0100

    Add JUnit Jupiter POM

--- src/main/java/tdd_java/App.java
+++ src/main/java/tdd_java/App.java
@@ -1,13 +1,9 @@
 package tdd_java;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
 }
